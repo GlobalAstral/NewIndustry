bf352e58aaec3a8bf59bdd5f4afb24151af955ee
GlobalAstral <cristianmainenti.03@gmail.com>
2026-04-05 23:40:05 +0200

    Added AlloySmelterBlockEntity logic

--- src/main/java/me/globalastral/newindustry/blockentities/custom/AlloySmelterBlockEntity.java
+++ src/main/java/me/globalastral/newindustry/blockentities/custom/AlloySmelterBlockEntity.java
@@ -3,16 +3,23 @@ package me.globalastral.newindustry.blockentities.custom;
 import me.globalastral.newindustry.blockentities.AbstractMachineBlockEntity;
 import me.globalastral.newindustry.blockentities.ModBlockEntities;
 import me.globalastral.newindustry.gui.alloy_smelter.AlloySmelterMenu;
+import me.globalastral.newindustry.recipe.AlloySmeltingRecipe;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public class AlloySmelterBlockEntity extends AbstractMachineBlockEntity implements MenuProvider {
 
@@ -68,7 +75,36 @@ public class AlloySmelterBlockEntity extends AbstractMachineBlockEntity implemen
 
     @Override
     public void tick(Level level, BlockPos pos, BlockState state) {
+        if (level.isClientSide())
+            return;
 
+        AlloySmeltingRecipe recipe = getRecipe();
+        if (recipe == null) {
+            resetProgress();
+            return;
+        }
+        incrementProgress();
+        setChanged(level, pos, state);
+        if (progress >= recipe.getProcess_time()) {
+            craftItem(recipe);
+            resetProgress();
+        }
+    }
+
+    private void craftItem(AlloySmeltingRecipe recipe) {
+        ItemStack result = recipe.getResultItem(RegistryAccess.EMPTY);
+        ItemStackHandler handler = this.getItemHandler();
+        handler.extractItem(INPUT_SLOT1, 1, false);
+        handler.extractItem(INPUT_SLOT2, 1, false);
+        handler.setStackInSlot(OUTPUT_SLOT, new ItemStack(result.getItem(), handler.getStackInSlot(OUTPUT_SLOT).getCount() + result.getCount()));
+    }
+
+    private void incrementProgress() {
+        progress++;
+    }
+
+    private void resetProgress() {
+        progress = 0;
     }
 
     @Override
@@ -80,5 +116,36 @@ public class AlloySmelterBlockEntity extends AbstractMachineBlockEntity implemen
     @Override
     public AbstractContainerMenu createMenu(int pContainerId, Inventory pPlayerInventory, Player pPlayer) {
         return new AlloySmelterMenu(pContainerId, pPlayerInventory, this, this.data);
+    }
+
+    private @Nullable AlloySmeltingRecipe getRecipe() {
+        Level level = this.level;
+        SimpleContainer inv = new SimpleContainer(this.getItemHandler().getSlots());
+        for (int i = 0; i < this.getItemHandler().getSlots(); i++) {
+            inv.setItem(i, this.getItemHandler().getStackInSlot(i));
+        }
+
+        Optional<AlloySmeltingRecipe> recipe = level.getRecipeManager()
+                .getRecipeFor(AlloySmeltingRecipe.Type.INSTANCE, inv, level);
+
+        if (recipe.isPresent()) {
+            AlloySmeltingRecipe rec = recipe.get();
+            ItemStack stack = rec.getResultItem(RegistryAccess.EMPTY);
+            if (canInsertAmount(inv, stack.getCount()) && canInsertItem(inv, stack)) {
+                this.max_progress = rec.getProcess_time();
+                return rec;
+            }
+        }
+        return null;
+    }
+
+    private static boolean canInsertItem(SimpleContainer inv, ItemStack stack) {
+        ItemStack itemStack = inv.getItem(OUTPUT_SLOT);
+        return itemStack.getItem() == stack.getItem() || itemStack.isEmpty();
+    }
+
+    private static boolean canInsertAmount(SimpleContainer inv, int amount) {
+        ItemStack stack = inv.getItem(OUTPUT_SLOT);
+        return stack.getCount() + amount <= stack.getMaxStackSize();
     }
 }
